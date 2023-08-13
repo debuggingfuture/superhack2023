cad9d1ed6fb6f9a6f0b63aa5661f1b86dfd3f541
debuggingfuture <lauchunyin@debuggingfuture.com>
2023-08-13 21:10:27 +0800

    add: fix ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -29,7 +29,21 @@ import { InjectedConnector } from 'wagmi/connectors/injected'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import Link from "next/link";
 import { ALCHEMY_API_KEY, WLD_ACTION_NAME, WLD_APP_ID } from "./webapp.config";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+
+// import { useNFT, useNFTMetadata } from '@zoralabs/nft-hooks'
+import { useNFT, Networks, NFTFetchConfiguration, Strategies } from '@zoralabs/nft-hooks';
+
+
+export const invokeApi = async (endpoint: string, reqBody: any): Promise<Response> => {
+  return await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(reqBody),
+  });
+}
 
 const { chains, publicClient } = configureChains(
   [mainnet, optimism, optimismGoerli, baseGoerli, zora],
@@ -75,15 +89,10 @@ const WorldCoinWidget = ({ addProof }) => {
       action: WLD_ACTION_NAME,
       signal: "",
     };
-    console.log("Sending proof to backend for verification:\n", JSON.stringify(reqBody)) // Log the proof being sent to our backend for visibility
-    const res: Response = await fetch("/api/verify", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(reqBody),
-    })
-    const data = await res.json()
+
+    const res = await invokeApi('/api/verify', reqBody);
+    const data = await res.json();
+
     if (res.status == 200) {
       console.log("Successful response from backend:\n", data); // Log the response from our backend for visibility
     } else {
@@ -114,6 +123,8 @@ const WorldCoinWidget = ({ addProof }) => {
 function Home() {
   const [score, setScore] = useState(0);
 
+
+
   const [proofs, setProofs] = useState([]);
 
   const addProof = (proof: any) => {
@@ -127,9 +138,25 @@ function Home() {
     address
   });
 
+  const { data: nftData, error } = useNFT('0x9fa2f3c62968119398d258d3ef8b34e050b997db', '1')
+
+  console.log('error', error);
+
+  const attestWithProofs = () => {
+
+  }
+
+  const onClickAttest = () => {
+
+  }
+
+  const worldCoinResult = useMemo(() => {
+    return _.find(proofs, p => p.type == 'worldcoin')
+  }, [proofs])
+
   useEffect(() => {
 
-    if (!isBalanceLoading && balanceData.value > 100000000000n) {
+    if (!isBalanceLoading && balanceData?.value > 100000000000n) {
       addProof({
         type: 'balance',
         value: 30
@@ -148,7 +175,7 @@ function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 text-white">
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-        <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
+        <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl mb-20">
           <span className="block bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent px-2">
             AutoMicroGrant
           </span>
@@ -157,7 +184,7 @@ function Home() {
 
         <section className="m-5">
 
-          <div className="text-white underline text-4xl m-2">
+          <div className="text-white underline text-4xl mb-4">
             <Link href="https://goerli-optimism.etherscan.io/address/0x37bc88f16e2ac014085feb977ce167fd9c6158639b6e79c027ce22de2455ada2" target="_blank">
               MicroGrant Contract: 0x123
             </Link>
@@ -186,7 +213,7 @@ function Home() {
           <div>
 
 
-            <h1 className="mx-auto text-center text-4xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
+            <h1 className="mx-auto text-center text-4xl mt-40 mb-30 font-extrabold tracking-tight text-white sm:text-7xl ">
               <span className="block bg-gradient-to-r from-red-500 to-green-500 bg-clip-text text-transparent px-2">
                 Reputation Score: <br />
                 {score}
@@ -212,15 +239,50 @@ function Home() {
                 We use WorldCoin to guarantee you can only grant 2 projects
               </h6>
               <div>
-                <WorldCoinWidget addProof={addProof} />
+                {
+                  worldCoinResult ? (
+                    <div className="text-green-500 text-2xl">
+                      You're qualified to attest!
+                    </div>
+                  ) : (
+                    <WorldCoinWidget addProof={addProof} /
+                    >)
+                }
               </div>
             </div>
 
 
             <div className="rounded border border-white p-10">
               <h3 className="text-2xl">
-                ☑️ NFT Ownership: +20 Optional
+                ☑️ 3D.ZORB NFT Ownership: +20 Optional
               </h3>
+              <span className="underline">
+                <Link target="_blank" href="https://zora.co/collect/zora:0x9fa2f3c62968119398d258d3ef8b34e050b997db/3">Zora</Link>
+              </span>
+
+              <div className="w-1/2 m-auto">
+                <img src="https://remote-image.decentralized-content.com/image?url=https%3A%2F%2Fipfs.decentralized-content.com%2Fipfs%2Fbafybeiavecwcmcdvquh3evaw7r4fslfgz3noavnipdzffjlg3phtwcpiom&w=1920&q=75" ></img>
+              </div>
+              {
+                _.find(proofs, p => p.type == 'nft') && (
+                  <>
+                    <div>
+                      Balance: {balanceData?.formatted} {balanceData?.symbol}
+                    </div>
+                    <div className="text-green-500 text-2xl"> +30 score!</div>
+                  </>
+                )
+              }
+
+
+              {
+                nftData && (<>
+                  <h3>{nftData.metadata.name}</h3>
+                  <p>{nftData.metadata.description}</p>
+                  <p>Owned by: {nftData.nft.owner.address}</p>
+                </>
+                )
+              }
 
             </div>
 
@@ -235,19 +297,23 @@ function Home() {
                   <div>  connect first! </div>
                 )
               }
-              <div>
-                Balance: {balanceData?.formatted} {balanceData?.symbol}
-              </div>
-              <div className="text-green-500 text-2xl"> +30 score!</div>
+              {
+                _.find(proofs, p => p.type == 'balance') && (
+                  <>
+                    <div>
+                      Balance: {balanceData?.formatted} {balanceData?.symbol}
+                    </div>
+                    <div className="text-green-500 text-2xl"> +30 score!</div>
+                  </>
+                )
+              }
+
 
 
 
             </div>
 
 
-            {/* <Link href="/attestor">
-            Attest
-          </Link> */}
 
 
 
@@ -256,7 +322,7 @@ function Home() {
 
         </section>
 
-        <section className="mt-20">
+        <section className="mt-20 mb-20">
           <h3 className="text-2xl">Proofs</h3>
           <code className="w-1/2 break-all">
             {JSON.stringify(proofs, null, 2)}
@@ -265,19 +331,18 @@ function Home() {
 
 
 
-        <section className="mt-50">
+        <section className="mt-50 w-full">
           <h1 className="text-4xl text-white">Attest for grantee</h1>
           <code>
             Submit and we will attest it on-chain for the receipent
           </code>
-          <h2 className="text-2xl text-white">Grantee</h2>
-          <div className="w-full max-w-xs">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="w-full rounded border border-white p-10">
+            <form className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="receipent">
+                <label className="block text-white text-lg font-bold mb-2" htmlFor="receipent">
                   receipent
                 </label>
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="receipent" type="text" placeholder="receipentaddress" />
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="receipent" type="text" placeholder="receipent address" />
               </div>
 
               <div className="flex items-center justify-between">
@@ -302,12 +367,17 @@ function Home() {
 }
 
 
-
+// https://github.com/ourzora/nft-hooks/blob/main/src/backends/zdk/utils/getChainFromNetwork.ts#L4
 export default function App() {
+  const strategy = Strategies.ZDKFetchStrategy
+  const zdkStrategy = new strategy('1');
   return (
     <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains}>
-        <Home />
+
+        <NFTFetchConfiguration strategy={zdkStrategy} networkId={Networks.MAINNET}>
+          <Home />
+        </NFTFetchConfiguration>
       </RainbowKitProvider>
     </WagmiConfig>
   );
